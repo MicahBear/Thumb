a8692b15b856fa2f90b00fac7c5a9258b05aa478
MicahBear <micah.b.elm@gmail.com>
2023-01-03 13:22:23 -0800

    create init pages

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,7 @@
+export function Dashboard() {
+    return (
+        <div className="dashboard-page">
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
---
+++ src/pages/First.js
@@ -0,0 +1,7 @@
+export function First() {
+    return (
+        <div className='first-page'>
+            <h1>Do you know your grow zone?</h1>
+        </div>
+    )
+}
