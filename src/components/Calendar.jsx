142349bb3ec61017415b8156a1453167b063b2c6
MicahBear <micah.b.elm@gmail.com>
2023-01-05 16:27:16 -0800

    working on wiring up basic functionality of btns and input on first page

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -32,21 +32,21 @@ export default function Calendar() {
         <tbody>
           <tr className="calendar--table-date-row text-center h-20">
             <td className="calendar--table-date border p-1 h-40 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto transition cursor-pointer duration-500 ease hover:bg-slate-500">
-              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 mx-auto overflow-hidden">
+              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 overflow-hidden">
                 <div className="calendar--date-num-container top h-5 w-full">
                   <span className="calendar--date-num text-gray-500">1</span>
                 </div>
               </div>
             </td>
             <td className="calendar--table-date border p-1 h-40 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto transition cursor-pointer duration-500 ease hover:bg-slate-500">
-              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 mx-auto overflow-hidden">
+              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 overflow-hidden">
                 <div className="calendar--date-num-container top h-5 w-full">
                   <span className="calendar--date-num text-gray-500">2</span>
                 </div>
               </div>
             </td>
             <td className="calendar--table-date border p-1 h-40 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto transition cursor-pointer duration-500 ease hover:bg-slate-500">
-              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 mx-auto overflow-hidden">
+              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10  overflow-hidden">
                 <div className="calendar--date-num-container top h-5 w-full">
                   <span className="calendar--date-num text-gray-500">3</span>
                 </div>
@@ -60,21 +60,21 @@ export default function Calendar() {
               </div>
             </td>
             <td className="calendar--table-date border p-1 h-40 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto transition cursor-pointer duration-500 ease hover:bg-slate-500">
-              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 mx-auto overflow-hidden">
+              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 overflow-hidden">
                 <div className="calendar--date-num-container top h-5 w-full">
                   <span className="calendar--date-num text-gray-500">5</span>
                 </div>
               </div>
             </td>
             <td className="calendar--table-date border p-1 h-40 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto transition cursor-pointer duration-500 ease hover:bg-slate-500">
-              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 mx-auto overflow-hidden">
+              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 overflow-hidden">
                 <div className="calendar--date-num-container top h-5 w-full">
                   <span className="calendar--date-num text-gray-500">6</span>
                 </div>
               </div>
             </td>
             <td className="calendar--table-date border p-1 h-40 xl:w-40 lg:w-30 md:w-30 sm:w-20 w-10 overflow-auto transition cursor-pointer duration-500 ease hover:bg-slate-500">
-              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 mx-auto overflow-hidden">
+              <div className="calendar--date-container flex flex-col h-40 mx-auto xl:w-40 lg:w-30 md:w-30 sm:w-full w-10 overflow-hidden">
                 <div className="calendar--date-num-container top h-5 w-full">
                   <span className="calendar--date-num text-gray-500">7</span>
                 </div>
--- src/pages/first.jsx
+++ src/pages/first.jsx
@@ -1,9 +1,29 @@
 export default function First() {
+  function dontKnow() {
+    // testing the dont know buttong is handled and outputs as f() says.
+    // all good
+    console.log("btn dont know was clicked");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // currently submit is handled by enter key from user. the onsubmit btn would cause a refresh. and the target would be incorrect.
+    // currently this console log out puts the value the user inputs.
+    console.log(e.target[0].value);
+  }
+
   return (
     <div className="first-page">
       <h2>What's your grow zone?</h2>
-      <input type="text" className="first--input" placeholder="Zone 8b.." />
-      <a className="first--btn">don't know</a>
+      <form onSubmit={handleSubmit}>
+        <input type="text" className="first--input" placeholder="Zone 8b.." />
+        {/*need addtional review*/}
+        {/* <a type="submit">lets grow</a> */}
+      </form>
+      <a className="first--btn" onClick={dontKnow}>
+        don't know
+      </a>
     </div>
   );
 }
