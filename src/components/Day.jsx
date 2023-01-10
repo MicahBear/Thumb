4b7e3685288801c42750948b23b133a00491267f
MicahBear <micah.b.elm@gmail.com>
2023-01-09 22:08:19 -0800

    back to composing date for STATE

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function Day() {
+export default function Day(date) {
   const renderDate = () => {
+    let days;
     const dateNow = new Date();
-    console.log(dateNow);
+    // need to change and state and create a possible utility.
+    for (let i = 0; i < 7; i++) {
+      dateNow.setDate(dateNow.getDate() + 1);
+      days = dateNow.toLocaleDateString();
+      console.log(days);
+      return days;
+    }
   };
   return <>{renderDate()}</>;
 }
