60c75d0e3a64bceabff1e73f47c58240785ebfae
MicahBear <micah.b.elm@gmail.com>
2023-01-31 14:34:57 -0800

    updating nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 export default function Nav() {
   return (
-    <div className="flex justify-around pt-3 dashboard--nav">
-      <i className="fa-solid fa-bars pl-3 "></i>
-      <h2 className="">Personal Hoe</h2>
-      <button className="">Logout</button>
-    </div>
+    <nav>
+      <div className="flex justify-around pt-3 dashboard--nav">
+        <i className="fa-solid fa-bars pl-3 "></i>
+        <Link to="/"> Personal Hoe </Link>
+        <button className="">Logout</button>
+      </div>
+    </nav>
   );
 }
